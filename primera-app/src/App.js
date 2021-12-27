a9002baf0574c7db668b35df27b23fae9f3250d1
Cachemarra <53624391+Cachemarra@users.noreply.github.com>
2021-12-26 18:23:02 -0700

    ACtualizando al video en 3_1

--- primera-app/src/App.js
+++ primera-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
+/*
+// Normal
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Bienvenidos a mi pág!</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Cachemarra"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visita mi Github!
         </a>
       </header>
     </div>
   );
 }
+*/
+
+// Operador flecha
+
+const App = () => {
+  <div>
+    <header>
+      <h1>Bienvenidos a mi pág!</h1>
+      <a
+        className="App-link"
+        href="https://github.com/Cachemarra"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Visita mi Github!
+      </a>
+    </header>
+  </div>
+};
 
 export default App;
