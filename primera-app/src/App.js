f9ebff184bbfe9fce6d22f87d75579aa56946d39
Cachemarra <53624391+Cachemarra@users.noreply.github.com>
2021-12-27 22:25:08 -0700

    3modulo concluído

--- primera-app/src/App.js
+++ primera-app/src/App.js
@@ -58,10 +58,9 @@ const App = () => ( // Creamos los cupcakes y sus elementos.
     <Cupcake color="azul" sabor="chocolate" foto="https://www.vanillacupcakery.com.au/wp-content/uploads/2018/04/IMG_2314-256x256.jpg"/>
     <Cupcake color="rosa" sabor="vainilla" foto="https://i.pinimg.com/600x315/b5/3c/dd/b53cdd7bebad3c60289176c831766d2d.jpg"/>
     <Cupcake color="azul" sabor="chocolate" foto="https://www.vanillacupcakery.com.au/wp-content/uploads/2018/04/IMG_2314-256x256.jpg"/>
-    <Cupcake color="rosa" sabor="vainilla" foto="https://i.pinimg.com/600x315/b5/3c/dd/b53cdd7bebad3c60289176c831766d2d.jpg"/>
-    <Cupcake color="azul" sabor="chocolate" foto="https://www.vanillacupcakery.com.au/wp-content/uploads/2018/04/IMG_2314-256x256.jpg"/>
-    <Cupcake color="rosa" sabor="vainilla" foto="https://i.pinimg.com/600x315/b5/3c/dd/b53cdd7bebad3c60289176c831766d2d.jpg"/>
-    <Cupcake color="azul" sabor="chocolate" foto="https://www.vanillacupcakery.com.au/wp-content/uploads/2018/04/IMG_2314-256x256.jpg"/>
+    <Cupcake color="morado" sabor="universo" foto="https://static.turbosquid.com/Preview/2020/02/16__08_13_24/RM.jpg7E362147-DD2F-4499-8793-604CB2957355Large.jpg"/>
+    <Cupcake color="oro" sabor="chocolomo" foto="https://www.hola.com/imagenes/cocina/recetas/20190910149038/cupcakes-chocolate-caramelo/0-717-246/cupcake-caramel-t.jpg"/>
+
   </div>
 );
 
--- primera-app/src/Cupcake.js
+++ primera-app/src/Cupcake.js
@@ -1,5 +1,6 @@
 // Creación del componente Cupcake
 
+/* // Declaración tipo función
 function Cupcake({ sabor, color, foto }){ // Props -> Propiedades.
     
     return ( // Retorna la estructura de los cupcakes
@@ -10,5 +11,54 @@ function Cupcake({ sabor, color, foto }){ // Props -> Propiedades.
         </div>
     )
 }
+*/
+
+// Declaración tipo clase
+
+import { Component } from 'react';
+
+class Cupcake extends Component {
+// Creamos los cupcakes, sus elementos y la lógica.
+
+    // Constructor
+    constructor(props) {
+        super(props);
+        // Creamos un estado para el componente.
+        this.state = {
+            vendido: false
+        }
+        // Lo bindeamos.
+        this.vender = this.vender.bind(this);
+    }
+
+    // Creamos un método para vender los cupcakes.
+    vender() {
+        // Recibe un objeto nuevo que reemplazara this.state.vendido
+        this.setState({
+            vendido: true
+        })
+    }
+
+
+    // Escribimos la lógica del componente
+    render() { 
+        console.log(this.state.vendido)
+        return(
+            <div>
+                <img src={ this.props.foto } alt={ this.props.sabor }/>
+                <h2>{ this.props.color} </h2>
+                <p>{ `Sabor: ${ this.props.sabor }` }</p>
+                <p>
+                    <b>Estado: </b>
+                    { this.state.vendido ? "Vendido": "A la venta" }</p>
+                {
+                    !this.state.vendido && <button onClick={ this.vender }>Comprar</button>
+                }
+                
+            </div>
+        )
+            // !this.state.vendido && <button>Comprar</button> -> Si no esta a la venta, desaparece el boton.
+    }
+} 
 
 export default Cupcake
