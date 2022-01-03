90ad228fa57ac969d46e771a513ccfaeec8f7243
Cachemarra <53624391+Cachemarra@users.noreply.github.com>
2022-01-03 15:13:06 -0700

    Se añadio botones y su funcionalidad. Falta: cambiar la disponibilidad si ya se vendio o reservo.

--- edcupcake/src/components/cards/Cupcake.js
+++ edcupcake/src/components/cards/Cupcake.js
@@ -1,24 +1,80 @@
 // Cartas para los cupcakes
-
+// Importo los estilos
+import "./style.css";
 // Importamos PropTypes para comprobar que los props sean correctos
 import { bool } from "prop-types";
 import { number, string } from "prop-types";
 
+// Importamos un hook para checar si se reservo o no y para tener referencia de la imagen.
+import { useState, useRef, useEffect } from "react";
+
 // Renderización del componente Cupcake
 const Cupcake = ({ descripcion, imagen, color, precio, sabor, disponible }) => {
+
+    // Configuramos los Hook
+    const [vendido, setVendido] = useState(false);
+    const [reservado, setReservado] = useState(false);
+
+    const refImagen = useRef();
+
+    // Función para vender
+    const vender = () => {
+        setVendido(true);
+        setReservado(true);
+
+        // Cambiamos el color de la imagen con un filtro a grises.
+        const elemento = refImagen.current;
+
+        elemento.classList.add("vendido");
+        elemento.classList.remove("reservado");
+    }
+
+
+    // Función para reservar
+    const reservar = () => {
+        setReservado(true);
+
+        const elemento = refImagen.current;
+        elemento.classList.add("reservado");
+    }
+
+    // UseEffect para checar si se vendio o no.
+    useEffect(() => {
+        const elemento = refImagen.current;
+        console.log(`Valor de elemento: ${elemento.classList}`);
+    }, [reservado, vendido]);
+
+
+    // Función para renderizar la sección de disponibilidad.
+    const renderDisponibilidad = (disponible) => {
+        // Comprobamos si existe 'disponible'
+        if (disponible) {
+            return(
+                <div>
+                    <p><b>Disponibilidad:</b> En existencia.</p>
+                    <button id="botonVender" onClick={ vender }>Comprar</button>
+                    <button id="botonReservar" onClick={ reservar }>Reservar</button>
+                </div>
+            )
+        }
+        else{
+            return(
+                <>
+                    <p><b>Disponibilidad:</b> Temporalmente agotado.</p>
+                </>
+            )
+        }
+    }
 
     return(
 
         <div className="s-radius-1 s-shadow-bottom background s-shadow-card-micro white-color s-pxy-2">
-            <img src={ imagen } alt={ sabor } />
+            <img src={ imagen } alt={ sabor } ref={ refImagen } />
             <p>{ descripcion }</p>
-            <span>Color: { color } </span>
-            <span>Precio: { precio }</span>
-            {console.log(disponible)}
-            {
-                <p> 
-                    Disponibilidad: {disponible? "En existencia.": "Temporalmente agotado." } 
-                </p>
+            <span><p><b>Color:</b> { color }</p></span>
+            <span><p><b>Precio:</b> { precio }</p></span>
+            {   
+                renderDisponibilidad(disponible)
             }
         </div>
     )
